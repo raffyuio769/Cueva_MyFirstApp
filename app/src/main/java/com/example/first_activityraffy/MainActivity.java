6def2285a54aefee546b292680f36dee3c8fd4d6
raffi1 <rafaelluiscueva545@gmail.com>
2023-03-10 15:42:38 +0800

    Cueva_myfirstapp

--- app/src/main/java/com/example/first_activityraffy/MainActivity.java
+++ app/src/main/java/com/example/first_activityraffy/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.first_activityraffy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.R.color;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView nme1;
+
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nme1 = (TextView) findViewById(R.id.Intro);
+        nme1.setTextColor(color.white);
     }
 }
